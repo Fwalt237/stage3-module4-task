a321e84e0fcae0b8c39992fa55b283dbf2b800d7
Fwalt237 <patrickkamwa17@gmail.com>
2025-12-04 13:32:21 +0100

    Added TestInstance

--- main-module/src/test/java/com/mjc/school/CommentControllerTest.java
+++ main-module/src/test/java/com/mjc/school/CommentControllerTest.java
@@ -4,6 +4,7 @@ import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
@@ -17,6 +18,7 @@ import static org.hamcrest.Matchers.*;
 @ActiveProfiles("test")
 @Sql(scripts = "/data.sql", executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
 @Sql(scripts = "/cleanup.sql", executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CommentControllerTest {
 
     @LocalServerPort
